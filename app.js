0f3174ccd7769b557b2e7d79e2f48ebefebaf716
Ayoub Nejjari <raysoflee07@gmail.com>
2017-05-10 14:34:18 +0100

    creating views and establishing the connection to Redis db

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const cons = require('consolidate');
 const bodyParser = require('body-parser');
 const path = require('path'); // nodeJs core module
 const redis = require('redis');
-const client = redis.createClient();
 const methodOverride = require('method-override');
 
 const app = express();
 // Port setting
 const PORT = 3000;
+
+let client = redis.createClient();
+
 
 // Tempate engine settings
 app.engine('html', cons.pug);
@@ -28,11 +30,27 @@ app.use(methodOverride('_method'));
 // call routes after the previous processing
 app.use(express.Router());
 
-app.get('/', function (req, res, next) {
-  res.render('main');
+
+// connect to Redis server
+client.on('connect', function() {
+  console.log('Server connected successfully!');
+  // Routes
+  /* Search page */
+  app.get('/', function (req, res, next) {
+    res.render('main');
+  });
+
+  /* Search processing */
+
+  app.post('/users/search', function(req, res, next) {
+    let id = req.body.id;
+  });
+
+
 });
+
 
 
 app.listen(PORT, function() {
   console.log('App is listening on port ' + PORT);
-})
+});
