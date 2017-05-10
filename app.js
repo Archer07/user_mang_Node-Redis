00e1da7cc474d4c5d608843efb65f42da0a74588
Ayoub Nejjari <raysoflee07@gmail.com>
2017-05-10 16:23:54 +0100

    rpost request to the database and creating simple UI

--- app.js
+++ app.js
@@ -33,21 +33,39 @@ app.use(express.Router());
 
 // connect to Redis server
 client.on('connect', function() {
-  console.log('Server connected successfully!');
-  // Routes
-  /* Search page */
-  app.get('/', function (req, res, next) {
-    res.render('main');
+  console.log('Redis connected successfully!');
+});
+
+// Routes
+/* Search page */
+app.get('/', function (req, res, next) {
+  res.render('main');
+});
+
+/* Search processing */
+
+app.post('/users/search', function(req, res, next) {
+  let id = req.body.id; // store the query data into the id variable
+  // once we have the id we can start using Redis
+
+  client.hgetall(id, function (err, hash) {
+    // check if the object didn't return
+    if (!hash) {
+      res.render('main', {
+        error: 'User doesn\'t exist!' // show error in case operation failed
+      });
+    } else {
+      //obj.id = id; // if user does exist, store the input id to obj.id
+       res.render('main', {
+        obj: hash,
+        message: 'User Has been retrived successfully!'
+       });
+    console.log(hash);
+    }
   });
-
-  /* Search processing */
-
-  app.post('/users/search', function(req, res, next) {
-    let id = req.body.id;
-  });
-
 
 });
+
 
 
 
